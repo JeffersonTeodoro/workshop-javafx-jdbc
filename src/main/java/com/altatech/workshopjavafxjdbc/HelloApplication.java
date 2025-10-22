3ff95b9ca08fa960059bad0aae7adfa7492bf115
Jefferson f. Teodoro <jeffersonfranca199@gmail.com>
2025-10-21 21:49:46 -0300

    Main view design

--- src/main/java/com/altatech/workshopjavafxjdbc/HelloApplication.java
+++ src/main/java/com/altatech/workshopjavafxjdbc/HelloApplication.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,9 +19,12 @@ public class HelloApplication extends Application {
     public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-            Parent parent = loader.load();
+            ScrollPane scrollPane = loader.load();
 
-            Scene mainScene = new Scene(parent);
+            scrollPane.setFitToHeight(true);
+            scrollPane.setFitToWidth(true);
+
+            Scene mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
