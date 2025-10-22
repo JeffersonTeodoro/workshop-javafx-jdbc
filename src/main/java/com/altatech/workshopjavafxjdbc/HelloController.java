2616cb6b4fec63b00d3b9b467e8aece39fb2548b
Jefferson f. Teodoro <jeffersonfranca199@gmail.com>
2025-10-21 22:05:09 -0300

    Main view controller

--- src/main/java/com/altatech/workshopjavafxjdbc/HelloController.java
+++ src/main/java/com/altatech/workshopjavafxjdbc/HelloController.java
@@ -1,14 +1,41 @@
 package com.altatech.workshopjavafxjdbc;
 
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private MenuItem manuItemSeller;
+
+    @FXML
+    private MenuItem manuItemDepartment;
+
+    @FXML
+    private MenuItem manuItemAbout;
+
+    @FXML
+    public void onManuItemSellerAction() {
+        System.out.println("onManuItemSellerAction");
+    }
+
+    @FXML
+    public void onManuItemDepartmentAction() {
+        System.out.println("onManuItemDepartmentAction");
+    }
+
+    @FXML
+    public void onManuItemAboutAction() {
+        System.out.println("onManuItemAboutAction");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
     }
 }
