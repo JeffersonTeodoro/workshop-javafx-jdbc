e7f02f0353bb46d1cf344fd8db41f0ff5cc4b75b
Jefferson f. Teodoro <jeffersonfranca199@gmail.com>
2025-10-25 07:40:51 -0300

    Department ComboBox

--- src/main/java/com/altatech/workshopjavafxjdbc/DepartmentListController.java
+++ src/main/java/com/altatech/workshopjavafxjdbc/DepartmentListController.java
@@ -106,6 +106,7 @@ public class DepartmentListController  implements Initializable, DataChangeListe
             dialogStage.showAndWait();
         }
         catch (IOException e) {
+            e.printStackTrace();
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(),  Alert.AlertType.ERROR);
         }
     }
